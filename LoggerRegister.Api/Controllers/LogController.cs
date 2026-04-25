f851ccd4e8edfb5fc9371510cc57a45c10147d36
Guilherme Surica Campos <eosurica@gmail.com>
2026-04-25 13:52:12 -0300

    fix: Alterações

--- LoggerRegister.Api/Controllers/LogController.cs
+++ LoggerRegister.Api/Controllers/LogController.cs
@@ -3,7 +3,6 @@ using LoggerRegister.Api.DTOs;
 using LoggerRegister.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 using RabbitMQ.Client;
-using System.Reflection.PortableExecutable;
 using System.Text;
 
 namespace LoggerRegister.Api.Controllers;
@@ -13,10 +12,46 @@ namespace LoggerRegister.Api.Controllers;
 public class LogController : ControllerBase
 {
     private readonly ApplicationDbContext _context;
-    private IConnection _connection;
-    private IChannel _channel;
+    private readonly ConnectionFactory _factory = new ConnectionFactory()
+    {
+        HostName = "localhost",
+        UserName = "admin",
+        Password = "admin"
+    };
 
-
+    private readonly List<CreateLoggerDTO> LogList = new List<CreateLoggerDTO>
+    {
+        new CreateLoggerDTO
+        {
+            Description = "INFO",
+            Name = "INFO",
+            Severity = Enums.Severity.INFO
+        },
+        new CreateLoggerDTO
+        {
+            Description = "INFO",
+            Name = "INFO",
+            Severity = Enums.Severity.WARNING
+        },
+        new CreateLoggerDTO
+        {
+            Description = "INFO",
+            Name = "INFO",
+            Severity = Enums.Severity.WARNING
+        },
+        new CreateLoggerDTO
+        {
+            Description = "INFO",
+            Name = "INFO",
+            Severity = Enums.Severity.INFO
+        },
+        new CreateLoggerDTO
+        {
+            Description = "INFO",
+            Name = "INFO",
+            Severity = Enums.Severity.CRITICAL
+        },
+    };
 
     public LogController(ApplicationDbContext context)
     {
@@ -24,44 +59,35 @@ public class LogController : ControllerBase
     }
 
     [HttpPost("create-log")]
-    public async Task<IActionResult> CreateLogger([FromBody] CreateLoggerDTO input)
+    public async Task<IActionResult> CreateLogger()
     {
         try
         {
-            var logModel = new LogModel(input.Name, input.Description, input.Severity);
-
-            if (logModel.Severity == Enums.Severity.ERROR || logModel.Severity == Enums.Severity.CRITICAL)
-            {
-                await _context.Set<LogModel>().AddAsync(logModel);
-                await _context.SaveChangesAsync();
-            }
-
-            await InstantiateRabbitMq();
+            await using var _connection = await _factory.CreateConnectionAsync();
+            await using var _channel = await _connection.CreateChannelAsync();
 
             await _channel.ExchangeDeclareAsync(exchange: "direct_logs", type: ExchangeType.Direct);
 
-            var body = Encoding.UTF8.GetBytes(logModel.Description);
+            foreach (var input in LogList)
+            {
+                var logModel = new LogModel(input.Name, input.Description, input.Severity);
 
-            await _channel.BasicPublishAsync(exchange: "direct_logs", routingKey: logModel.Severity.ToString(), body: body);
+                if (logModel.Severity == Enums.Severity.ERROR || logModel.Severity == Enums.Severity.CRITICAL)
+                {
+                    await _context.Set<LogModel>().AddAsync(logModel);
+                    await _context.SaveChangesAsync();
+                }
 
-            return Ok(logModel);
-           
+                var body = Encoding.UTF8.GetBytes(logModel.Description);
+
+                await _channel.BasicPublishAsync(exchange: "direct_logs", routingKey: logModel.Severity.ToString(), body: body);
+            }
+
+            return Ok();
+
         } catch (Exception)
         {
             return BadRequest();
-        } 
-    }
-
-    private async Task InstantiateRabbitMq()
-    {
-        var factory = new ConnectionFactory()
-        {
-            HostName = "localhost",
-            UserName = "admin",
-            Password = "admin"
-        };
-
-        _connection = await factory.CreateConnectionAsync();
-        _channel = await _connection.CreateChannelAsync();
+        }
     }
 }
